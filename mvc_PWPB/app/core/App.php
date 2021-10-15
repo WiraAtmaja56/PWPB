27a8e44408aa8959b917034565bf0d46571136bb
WiraAtmaja56 <madewira772@gmail.com>
2021-10-15 22:05:55 +0800

    Tambah Rooting

--- mvc_PWPB/app/core/App.php
+++ mvc_PWPB/app/core/App.php
@@ -3,8 +3,18 @@
 class App
 {
 
+    public function parseURL() {
+        if(isset($_GET["url"])) {
+            $url = rtrim($_GET["url"], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode("/", $url);
+            return $url;
+        }
+    }
+
     public function __construct() {
-        echo "Web MVC Siap Dijalankan!";
+        $url = $this->parseURL();
+        var_dump($url);
     }
 
 }
