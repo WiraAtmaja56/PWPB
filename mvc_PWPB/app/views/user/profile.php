67c3f80284c600632643add2bf3269c76af8b1dc
WiraAtmaja56 <madewira772@gmail.com>
2021-12-22 13:35:26 +0800

    Tambah Asset

--- mvc_PWPB/app/views/user/profile.php
+++ mvc_PWPB/app/views/user/profile.php
@@ -1,15 +1,16 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= BASE_URL; ?>/css/bootstrap.css">
     <title>Halaman <?= $data['judul']; ?></title>
 </head>
 <body>
-    <h1>Halaman User</h1>
-    <p>Halo, nama saya <?=  $data['nama']; ?>, saya seorang <?=  $data['pekerjaan']; ?><p>
+        <div class="container text-center mt-4">
+            <h1>Halaman User</h1><img src="<?= BASE_URL; ?>/img/Profile.png" class="rounded-circle shadow">
+            <p>Halo, nama saya <?= $data["nama"]; ?>, saya seorang <?= $data["pekerjaan"];?></p>
+        </div>
 </body>
 </html>
